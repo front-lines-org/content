8fb2d291d4fb6a3ffdc2a29c69715e8a1532daa9
rubennaatje <ruben.soerdien@hotmail.com>
2022-01-30 22:33:52 +0000

    Deploying to gh-pages from @ front-lines-org/content@2edd04dfa9f7d18990d17fdeb092ede2efeed626 🚀

---
+++ _nuxt/static/1643582030/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643582030",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a00e4899"}}}}(null,"\u002F"));
